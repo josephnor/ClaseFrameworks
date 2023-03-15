64775d366c7d435552c60c7e8128fca32e578531
josephnor <sebas1006a15@gmail.com>
2023-03-15 15:55:32 -0500

    15/03/2023

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesion</title>
-@vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
 </head>
 <body>
@@ -29,9 +29,15 @@
                     @csrf
 
                     @if(session('mensaje'))
-                      <p class= "bg-red-600 text-white my-2 text-sm p-2 text-center">
-                        {{session('mensaje')}}
-                      </p>
+                      <!--<p class= "bg-red-600 text-white my-2 text-sm p-2 text-center">-->
+                        <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                          <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                          <span class="sr-only"></span>
+                          <div>
+                            <span class="font-extrabold"></span>{{session('mensaje')}}
+                          </div>
+                        </div>
+                         <!--</p>-->
                     @endif
 
                 <div class="flex flex-row items-center justify-center lg:justify-start">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -13,5 +14,7 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
